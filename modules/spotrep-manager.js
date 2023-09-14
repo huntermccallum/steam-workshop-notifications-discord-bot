4d9db1c9338559d22efd3f8f9157d04510d20315
Hunter McCallum <46570998+huntermccallum@users.noreply.github.com>
2023-09-14 08:50:58 -0600

    Refactor spotrep-manager.js for improved readability
    - Transitioned from promise chaining to async/await for better code clarity.
    - Simplified error handling by consolidating try/catch blocks in the checkSpotRep event handler.
    - Removed nested asynchronous operations to reduce code complexity.

--- modules/spotrep-manager.js
+++ modules/spotrep-manager.js
@@ -1,90 +1,82 @@
-import EventEmitter from 'events'
-import moment from 'moment'
-import * as cache from './cache-manager.js'
-import * as util from './util.js'
-import { modManager } from './mod-manager.js'
-import { logger } from './logger.js'
+import EventEmitter from 'events';
+import moment from 'moment';
+import * as cache from './cache-manager.js';
+import * as util from './util.js';
+import { modManager } from './mod-manager.js';
+import { logger } from './logger.js';
 
-const spotRepManager = new EventEmitter()
-let spotRepDatabase = new Map()
+const spotRepManager = new EventEmitter();
+let spotRepDatabase = new Map();
 
 spotRepManager.on('loadSpotRepFromCache', (cachedSpotRep) => {
-    logger.info('Loading SpotRep from cache.')
-
-    spotRepDatabase = new Map(Object.entries(cachedSpotRep))
-
-    logger.info(`SpotRep loaded from cache, last update: ${spotRepDatabase.get('lastUpdate')}.`)
-})
+    logger.info('Loading SpotRep from cache.');
+    spotRepDatabase = new Map(Object.entries(cachedSpotRep));
+    logger.info(`SpotRep loaded from cache, last update: ${spotRepDatabase.get('lastUpdate')}.`);
+});
 
 spotRepManager.on('saveSpotRepToCache', async () => {
-    logger.info('Saving SpotRep to cache.')
-
+    logger.info('Saving SpotRep to cache.');
     try {
-        await cache.writeSpotRepToCache(Object.fromEntries(spotRepDatabase))
+        await cache.writeSpotRepToCache(Object.fromEntries(spotRepDatabase));
     } catch (e) {
-        logger.error(e)
+        logger.error(e);
     }
-})
+});
 
 spotRepManager.on('checkSpotRep', async (client) => {
-    let spotRepData
     try {
-        spotRepData = await util.downloadFile('https://dev.arma3.com/spotrep').then((html) => util.parseArmaSpotRepHtml(html))
-    } catch (e) {
-        logger.error(`Could not parse ArmA SpotRep: ${e}`)
-        return
-    }
+        const spotRepData = await util.downloadFile('https://dev.arma3.com/spotrep');
+        const parsedData = await util.parseArmaSpotRepHtml(spotRepData);
 
-    if (spotRepData['time'].isBefore(moment(spotRepDatabase.get('lastUpdate')))) {
-        logger.debug('SpotRep not updated')
-        return
-    }
+        if (parsedData['time'].isBefore(moment(spotRepDatabase.get('lastUpdate')))) {
+            logger.debug('SpotRep not updated');
+            return;
+        }
 
-    logger.info('SpotRep updated')
-    spotRepDatabase.set('lastUpdate', moment().format())
+        logger.info('SpotRep updated');
+        spotRepDatabase.set('lastUpdate', moment().format());
 
-    let changelog
-    try {
-        changelog = await util.downloadFile(spotRepData['link']).then((html) => util.parseArmaSpotRepPostHtml(html))
-    } catch (e) {
-        logger.error(`Could not parse ArmA SpotRep changelog: ${e}`)
-        return
-    }
+        const changelog = await util.downloadFile(parsedData['link']);
+        const parsedChangelog = await util.parseArmaSpotRepPostHtml(changelog);
 
-    modManager.emit('listNotifications', async (notificationsMap) => {
-        for (const [guildId, notifications] of notificationsMap.entries()) {
-            let notificationsString
-            try {
-                notificationsString = await util.buildNotificationString(notifications, guildId, client)
-            } catch (e) {
-                logger.error(`Could not build notification string for guild ID '${guildId}': ${e}`)
-                continue
-            }
+        modManager.emit('listNotifications', async (notificationsMap) => {
+            for (const [guildId, notifications] of notificationsMap.entries()) {
+                let notificationsString;
+                try {
+                    notificationsString = await util.buildNotificationString(notifications, guildId, client);
+                } catch (e) {
+                    logger.error(`Could not build notification string for guild ID '${guildId}': ${e}`);
+                    continue;
+                }
 
-            const message = `Arma was updated \`${spotRepData['info']}\` (<${spotRepData['link']}>) was updated! ${notificationsString}`
-            const splitChangelog = util.splitString(changelog, 1900, '\n')
+                const message = `Arma was updated \\`${parsedData['info']}\\` (<${parsedData['link']}>) was updated! ${notificationsString}`;
+                const splitChangelog = util.splitString(parsedChangelog, 1900, '\\n');
 
-            for (const channelId of notifications.channelIds) {
-                client.channels.cache
-                    .get(channelId)
-                    .send(message)
-                    .catch((e) => logger.error(e))
-
-                for (let changelogPart of splitChangelog) {
-                    if (changelogPart.length === 0) continue
-
-                    changelogPart = `\`\`\`${changelogPart}\`\`\``
-
+                for (const channelId of notifications.channelIds) {
                     client.channels.cache
                         .get(channelId)
-                        .send(changelogPart)
-                        .catch((e) => logger.error(e))
+                        .send(message)
+                        .catch((e) => logger.error(e));
+
+                    for (let changelogPart of splitChangelog) {
+                        if (changelogPart.length === 0) continue;
+
+                        changelogPart = `\`\`\`${changelogPart}\`\`\``;
+
+                        client.channels.cache
+                            .get(channelId)
+                            .send(changelogPart)
+                            .catch((e) => logger.error(e));
+                    }
                 }
             }
-        }
-    })
+        });
 
-    spotRepManager.emit('saveSpotRepToCache')
-})
+        spotRepManager.emit('saveSpotRepToCache');
 
-export { spotRepManager }
+    } catch (e) {
+        logger.error(`Error checking SpotRep: ${e.message}`);
+    }
+});
+
+export { spotRepManager };
