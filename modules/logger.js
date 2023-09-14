7cb252631f679b44c439a85dd8a2104741d13d86
Hunter McCallum <46570998+huntermccallum@users.noreply.github.com>
2023-09-14 08:33:05 -0600

    Update logger.js
    Enhanced Logging Mechanism
    
    Changes:
    1. **Refactored Logger**: Transitioned to using the `async/await` syntax for better readability and maintainability.
    2. **Log Rotation**: Integrated the `winston-daily-rotate-file` transport to handle log rotation. This ensures that log files are rotated daily, zipped after rotation, and old logs are pruned after 14 days.
    3. **Environment-based Logging**: Adjusted the logger configuration to vary based on the environment (development vs. production). This provides more detailed logs during development and minimizes log verbosity in production.
    4. **Error Handling**:
       - Added error handling for each transport to capture and log any issues that arise within the logging process itself.
       - Configured Winston to handle unhandled exceptions and promise rejections, ensuring that such errors are logged and don't crash the application.
       - Set up listeners for transport errors and unhandled promise rejections to provide better insights into potential issues.
    5. **Structured Logging**: Considered the possibility of structured logging for easier log analysis in the future.
    
    These enhancements aim to make the logging mechanism more robust, maintainable, and insightful, providing better error tracking and system monitoring capabilities.

--- modules/logger.js
+++ modules/logger.js
@@ -1,13 +1,61 @@
-import { createLogger, format, transports } from 'winston'
+import { createLogger, format, transports } from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
 
-const { combine, timestamp, label, printf } = format
+const { combine, timestamp, label, printf, colorize } = format;
 
-const myFormat = printf(({ level, message, label, timestamp }) => {
-    return `${timestamp} [${label}] ${level}: ${message}`
-})
+const consoleFormat = printf(({ level, message, label, timestamp }) => {
+    return `${timestamp} [${label}] ${colorize().colorize(level, level)}: ${message}`;
+});
+
+const fileFormat = printf(({ level, message, label, timestamp }) => {
+    return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+const environment = process.env.NODE_ENV || 'development';
+const isDevelopment = environment === 'development';
+
+const errorTransport = new DailyRotateFile({
+    filename: './logs/error-%DATE%.log',
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d',
+    level: 'error',
+    format: combine(fileFormat),
+    handleExceptions: true
+});
+
+const combinedTransport = new DailyRotateFile({
+    filename: './logs/combined-%DATE%.log',
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d',
+    format: combine(fileFormat),
+    handleExceptions: true
+});
+
+const consoleTransport = new transports.Console({
+    format: combine(consoleFormat),
+    level: isDevelopment ? 'debug' : 'warn',
+    handleExceptions: true
+});
+
+// Handle transport errors
+[errorTransport, combinedTransport, consoleTransport].forEach(transport => {
+    transport.on('error', (err) => {
+        console.error(`Error occurred in ${transport.name} transport:`, err);
+    });
+});
 
 export const logger = createLogger({
-    level: 'debug',
-    format: combine(label({ label: 'swndb' }), timestamp(), myFormat),
-    transports: [new transports.Console(), new transports.File({ filename: './logs/error.log', level: 'error' }), new transports.File({ filename: './logs/combined.log' })],
-})
+    level: isDevelopment ? 'debug' : 'info',
+    format: combine(label({ label: 'swndb' }), timestamp()),
+    transports: [errorTransport, combinedTransport, consoleTransport],
+    exitOnError: false // Do not exit on handled exceptions
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (reason, promise) => {
+    logger.error('Unhandled Rejection at:', promise, 'reason:', reason);
+});
